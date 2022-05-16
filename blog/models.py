568eb9fe59b645d444202a69fe08da59b2e43607
Sayan Biswas <sayan@pokurt.me>
2022-05-16 19:13:41 +0530

    Implement user/post creation, editing and password resets
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
+import typing
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.utils import timezone
 
 class Post(models.Model):
@@ -7,6 +9,11 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # image = models.ImageField()
     
-    def __str__(self) -> str:
+    def __str__(self) -> typing.Text:
         return self.title
+    
+    def get_absolute_url(self) -> typing.Text:
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
--- django_project/settings.py
+++ django_project/settings.py
@@ -10,8 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import json
 import os
 from pathlib import Path
+import typing
+
+from pydantic import BaseModel
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +134,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+
+
+class FileConfig(BaseModel):
+    email_addr: typing.Text
+    email_pass: typing.Text
+    
+conf: FileConfig
+
+with open('config.json', "r") as f:
+    conf=FileConfig(**json.loads(f.read()))
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = conf.email_addr
+EMAIL_HOST_PASSWORD = conf.email_pass
